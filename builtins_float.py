2b360aa85541361caf127d3a6443158d2a270e59
White7Jane <Jane_fau_yzh@163.com>
2017-06-15 17:02:16 +0800

    Create builtins_float.py

---
+++ builtins_float.py
@@ -0,0 +1,151 @@
+class float(object):
+    """Floating point numeric type."""
+
+    def __init__(self, x=None):
+        """Convert a string or a number to floating point.
+
+        :type x: object
+        """
+        pass
+
+    def __add__(self, y):
+        """Sum of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __sub__(self, y):
+        """Difference of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __mul__(self, y):
+        """Product of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __floordiv__(self, y):
+        """Floored quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __mod__(self, y):
+        """Remainder of x / y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __pow__(self, y):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __div__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __truediv__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __radd__(self, y):
+        """Sum of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __rsub__(self, y):
+        """Difference of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __rmul__(self, y):
+        """Product of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __rfloordiv__(self, y):
+        """Floored quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __rmod__(self, y):
+        """Remainder of y / x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __rpow__(self, y):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __rdiv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __rtruediv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        return 0.0
+
+    def __pos__(self):
+        """x unchanged.
+
+        :rtype: float
+        """
+        return 0.0
+
+    def __neg__(self):
+        """x negated.
+
+        :rtype: float
+        """
+        return 0.0
