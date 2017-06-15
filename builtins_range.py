362a9a634e4dac1672ba276885d17410c6d0cc4f
White7Jane <Jane_fau_yzh@163.com>
2017-06-15 17:04:03 +0800

    Create builtins_range.py

---
+++ builtins_range.py
@@ -0,0 +1,12 @@
+class range(object):
+    """range object."""
+
+    def __init__(self, start, stop=None, step=None):
+        """Create a range object.
+
+        :type start: numbers.Integral
+        :type stop: numbers.Integral | None
+        :type step: numbers.Integral | None
+        :rtype: range[int]
+        """
+        pass
