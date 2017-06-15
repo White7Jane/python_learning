e574cdffc737e08787d4d9694463ab7186730856
White7Jane <Jane_fau_yzh@163.com>
2017-06-15 17:03:31 +0800

    Create builtins_int.py

---
+++ builtins_int.py
@@ -0,0 +1,241 @@
+class int(object):
+    """Integer numeric type."""
+
+    def __init__(self, x=None, base=10):
+        """Convert a number or string x to an integer, or return 0 if no
+        arguments are given.
+
+        :type x: object
+        :type base: numbers.Integral
+        """
+        pass
+
+    def __add__(self, y):
+        """Sum of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __sub__(self, y):
+        """Difference of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __mul__(self, y):
+        """Product of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __floordiv__(self, y):
+        """Floored quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __mod__(self, y):
+        """Remainder of x / y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __pow__(self, y, modulo=None):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :type modulo: numbers.Integral | None
+        :rtype: int
+        """
+        return 0
+
+    def __lshift__(self, n):
+        """x shifted left by n bits.
+
+         :type n: numbers.Integral
+         :rtype: int
+         """
+        return 0
+
+    def __rshift__(self, n):
+        """x shifted right by n bits.
+
+         :type n: numbers.Integral
+         :rtype: int
+         """
+        return 0
+
+    def __and__(self, y):
+        """Bitwise and of x and y.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        return 0
+
+    def __or__(self, y):
+        """Bitwise or of x and y.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        return 0
+
+    def __xor__(self, y):
+        """Bitwise exclusive or of x and y.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        return 0
+
+    def __div__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __truediv__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __radd__(self, y):
+        """Sum of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __rsub__(self, y):
+        """Difference of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __rmul__(self, y):
+        """Product of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __rfloordiv__(self, y):
+        """Floored quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __rmod__(self, y):
+        """Remainder of y / x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __rpow__(self, y):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __rlshift__(self, y):
+        """y shifted left by x bits.
+
+         :type y: numbers.Integral
+         :rtype: int
+         """
+        return 0
+
+    def __rrshift__(self, y):
+        """y shifted right by n bits.
+
+         :type y: numbers.Integral
+         :rtype: int
+         """
+        return 0
+
+    def __rand__(self, y):
+        """Bitwise and of y and x.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        return 0
+
+    def __ror__(self, y):
+        """Bitwise or of y and x.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        return 0
+
+    def __rxor__(self, y):
+        """Bitwise exclusive or of y and x.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        return 0
+
+    def __rdiv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __rtruediv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        return 0
+
+    def __pos__(self):
+        """x unchanged.
+
+        :rtype: int
+        """
+        return 0
+
+    def __neg__(self):
+        """x negated.
+
+        :rtype: int
+        """
+        return 0
+
+    def __invert__(self):
+        """The bits of x inverted.
+
+        :rtype: int
+        """
+        return 0
